ddba4d419bcb09954297f8f4aae28cf9ff80b1d7
Avival <chrisFJ.avival@gmail.com>
2025-06-18 17:14:02 -0600

    Implemented paginationFormat method in dataFormats.js -> it gives a proper pagination format to the given data.

---
+++ source/utilities/responses/dataFormats.js
@@ -0,0 +1,18 @@
+const { paginationConstants } = require( '/app/source/utilities/constants/paginationConstants' )
+
+function paginationFormat( data, paginationParameters, totalCount ) {
+    const totalPages = parseInt( totalCount / ( paginationParameters.limit ?? paginationConstants.DEFAULT_LIMIT ) )
+    const offsetPage = totalCount % ( paginationParameters.limit ?? paginationConstants.DEFAULT_LIMIT ) != 0
+
+    return {
+        current_page: paginationParameters.page ?? 1,
+        data: data,
+        per_page: paginationParameters.limit ?? paginationConstants.DEFAULT_LIMIT,
+        total_entries: totalCount,
+        total_pages: offsetPage ? totalPages + 1 : totalPages
+    }
+}
+
+module.exports = {
+    paginationFormat
+}
