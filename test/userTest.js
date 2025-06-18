39c97ef2f8f5c174ac10cb13d812e62ccc35d74d
Avival <chrisFJ.avival@gmail.com>
2025-06-17 23:00:46 -0600

    Added Moca framework and Chai library for future testing. Added initial tests for User endpoint components. Updated .env.example file.

---
+++ test/userTest.js
@@ -0,0 +1,82 @@
+const assert = require( "chai" ).assert
+
+describe( "User Test Suite", function() {
+    describe( "User Repository Tests", function() {
+        it( "User Repository should exist", function() {
+            assert.exists( repository )
+        } )
+
+        it( "'Delete' method should exist.", function () {
+            assert.exists( repository.delete )
+            assert.isFunction( repository.delete )
+        } )
+
+        it( "'Get' method should exist.", function () {
+            assert.exists( repository.get )
+            assert.isFunction( repository.get )
+        } )
+
+        it( "'List' method should exist.", function () {
+            assert.exists( repository.list )
+            assert.isFunction( repository.list )
+        } )
+
+        it( "'Create' method should exist.", function () {
+            assert.exists( repository.create )
+            assert.isFunction( repository.create )
+        } )
+
+        it( "'Update' method should exist.", function () {
+            assert.exists( repository.update )
+            assert.isFunction( repository.update )
+        } )
+    } )
+
+    describe( "User Request Tests", function() {
+        it( "Delete user request should exist", function() {
+            assert.exists( deleteUserRequest )
+        } )
+
+        it( "Index user request should exist", function() {
+            assert.exists( indexUserRequest )
+        } )
+
+        it( "Show user request should exist", function() {
+            assert.exists( showUserRequest )
+        } )
+
+        it( "Store user request should exist", function() {
+            assert.exists( storeUserRequest )
+        } )
+
+        it( "Update user request should exist", function() {
+            assert.exists( updateUserRequest )
+        } )
+    } )
+
+    describe( "User Controller Tests", function() {
+        it( "User Controller should exist", function() {
+            assert.exists( controller )
+        } )
+
+        it( "'Delete' method should exist.", function () {
+            assert.exists( controller.delete )
+            assert.isFunction( controller.delete )
+        } )
+
+        it( "'Get' method should exist.", function () {
+            assert.exists( controller.get )
+            assert.isFunction( controller.get )
+        } )
+
+        it( "'Post' method should exist.", function () {
+            assert.exists( controller.list )
+            assert.isFunction( controller.list )
+        } )
+
+        it( "'Put' method should exist.", function () {
+            assert.exists( controller.put )
+            assert.isFunction( controller.put )
+        } )
+    } )
+} )
