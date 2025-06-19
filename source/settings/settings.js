33e6757af356be874f7cb492faff147a4e4fe302
Avival <chrisFJ.avival@gmail.com>
2025-06-19 11:22:31 -0600

    Implemented settings configuration file.

---
+++ source/settings/settings.js
@@ -0,0 +1,8 @@
+const settings = {
+    error_logging: "normal", //I'm thinking "off", "normal", "verbose"
+    language: "en" // "en" or "es" at the moment
+}
+
+module.exports = {
+    settings
+}
