2bc04e5aafb94cc7d7199be3b04f655b182a8732
Avival <chrisFJ.avival@gmail.com>
2025-06-19 19:29:46 -0600

    Implemented regex constants -> will be used in future validation schemas. Renamed paginationConstants and validationConstants -> removed redundant part of the file names.

---
+++ source/utilities/constants/regex.js
@@ -0,0 +1,16 @@
+const regex = Object.freeze( {
+	address_regex: '^[a-zA-ZÀ-ž0-9,.\' ]*$',
+	comment_regex: '^[a-zA-ZÀ-ž0-9!\/,\'\\?\\n._ :+#\-]*$',
+	date_regex: '^[0-9\-: ]*$',
+	email_regex: '^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]$',
+	only_letters_regex: '^[a-zA-ZÀ-ž ]*$',
+	general_use_regex: '^[a-zA-ZÀ-ž0-9!\/,\'\\?._ :+#\-]*$',
+	general_use_regex_no_numbers: '^[a-zA-ZÀ-ž!\/,\'\\?._ :+#\-]*$',
+	name_regex: '^[a-zA-ZÀ-ž ]*$',
+	password_regex: '^[a-zA-ZÀ-ž0-9!@#\$%\^\&*\-+._]*$',
+	phone_regex: '^[0-9+ ]*$',
+	time_regex: '^[0-9:]*$',
+	year_regex: '^[0-9\-]*$',
+} )
+
+module.exports = { regex }
