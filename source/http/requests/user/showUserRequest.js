6c6e5bad9e7b50accfa2442b176e90523411ad0c
Avival <chrisFJ.avival@gmail.com>
2025-06-24 20:17:42 -0600

    Removed unnecessary code.

--- source/http/requests/user/showUserRequest.js
+++ source/http/requests/user/showUserRequest.js
@@ -5,7 +5,6 @@ const languageStrings = getLanguageStrings( settings.language )
 const userSchemas = require( "/app/source/http/validationSchemas/user" )
 
 async function showUserRequest( request, response, next ) {
-    console.log( request.query )
     try {
         const parameters = request.params
         const query = request.query
@@ -13,7 +12,6 @@ async function showUserRequest( request, response, next ) {
         await userSchemas.showUserQuerySchema.validate( query )
         next()
     } catch( validationError ) {
-        console.log( validationError )
         response.status( 422 ).send( errorResponse( 422, languageStrings.errorMessages.invalid_user_input, validationError.errors[ 0 ] + " ( " + validationError.path + " )" ) )
     }
 }
