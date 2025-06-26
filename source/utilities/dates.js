f023d950dbc366db7d7cfb8a6e34fdf45ef60230
Avival <chrisFJ.avival@gmail.com>
2025-06-25 21:39:45 -0600

    Implemented method isDateExpired that checks whether given date is 'expired' when compared to target date.

--- source/utilities/dates.js
+++ source/utilities/dates.js
@@ -1,3 +1,5 @@
+const { DateTime, Duration } = require( 'luxon' )
+
 function getOffsetDate( offsetDays ) {
     const currentDate = new Date()
     const offsetDate = new Date()
@@ -5,6 +7,15 @@ function getOffsetDate( offsetDays ) {
     return offsetDate
 }
 
+function isDateExpired( dateToCheck, targetDate ) {
+    const test = DateTime.fromISO( dateToCheck.replace( " ", "T" ) )
+    const limit = DateTime.fromISO( targetDate.replace( " ", "T" ) )
+    const difference = Duration.fromObject( test.diff( limit, [ 'years', 'months', 'days' ] ).toObject() )
+
+    return Number( process.env.DEFAULT_PASSWORD_EXPIRATION_DAY_LENGTH ) - difference.as( 'days' ) > Number( process.env.DEFAULT_PASSWORD_EXPIRATION_DAY_LENGTH )
+}
+
 module.exports = {
-    getOffsetDate
+    getOffsetDate,
+    isDateExpired
 }
