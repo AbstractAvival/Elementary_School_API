4f3969963ecc28cb5dee277ca5e6dc5e2ecdb5cc
Avival <chrisFJ.avival@gmail.com>
2025-06-25 18:27:35 -0600

    Implemented initial schema column constants. Will replace constant values in all validation schemas.

---
+++ source/utilities/constants/schemaColumn.js
@@ -0,0 +1,8 @@
+const schemaColumnConstants = Object.freeze( {
+    DEFAULT_MAX_ID_OR_USERNAME_VALUE: 30,
+    DEFAULT_MAX_PASSWORD_VALUE: 30
+} )
+
+module.exports = {
+    schemaColumnConstants
+}
